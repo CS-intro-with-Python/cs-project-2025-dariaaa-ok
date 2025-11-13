e737cd73fb9f2d0787aa5f01814042b75448e892
Daria <dakizilova@edu.aau.at>
2025-11-13 18:56:56 +0200

    Changed the client to use workflow commands (::notice, ::error) and colored output to display successful and unsuccessful results

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import sys
 
 import requests
 
+OK, ERR, RST = "\033[32m", "\033[31m", "\033[0m"
 
 response = requests.get("http://127.0.0.1:8080/")
 print(response.text)
@@ -10,8 +11,11 @@ response_code = response.status_code
 print(response_code)
 
 if response_code == 200:
-    print("All fine!")
+    print(f"{OK}All fine!{RST}")
+    print("::notice title=Healthcheck passed::")
 else:
+    print(f"{ERR}Something went wrong!{RST}")
+    print("::error title=Healthcheck failed::")
     sys.exit(1)
 
 
