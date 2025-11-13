fe83b0a1ab0eca6d826995f8f700f8e713ff1f4e
Daria <dakizilova@edu.aau.at>
2025-11-13 21:22:57 +0200

    Changed the route

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ OK, ERR, RST = "\033[32m", "\033[31m", "\033[0m"
 
 base_url = "https://cs-project-2025-dariaaa-ok-production.up.railway.app"
 
-routes = ["/error", "/home", "/animals", "/animals/dogs", "/animals/cats"]
+routes = ["/", "/home", "/animals", "/animals/dogs", "/animals/cats"]
 
 for route in routes:
     response = requests.get(base_url + route)
