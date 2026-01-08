41c97433ed595e74dc26c96411c19ef4bef2e8f4
Daria <dakizilova@edu.aau.at>
2026-01-08 16:57:01 +0200

    Changed the base url

--- client.py
+++ client.py
@@ -3,7 +3,8 @@ import requests
 
 OK, ERR, RST = "\033[32m", "\033[31m", "\033[0m"
 
-base_url = "https://cs-project-2025-dariaaa-ok-production.up.railway.app"
+base_url = "http://127.0.0.1:8080"
+    # "https://cs-project-2025-dariaaa-ok-production.up.railway.app"
 
 routes = ["/", "/dogs", "/cats"]
 
